056a784373c71493a97021658a6d6ed713554700
matko <mabutko@riteh.hr>
2019-03-13 12:10:35 +0100

    OperationLogic V2

---
+++ CalculatorGroup2/CalculatorGroup2/OperationAddition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+    public class OperationAddition
+    {
+        public float PerformAddition(float nb1, float nb2)
+        {
+            return nb1 + nb2;
+        }
+
+    internal float PerformAddition(string v1, string v2)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+    public class OperationSubstraction
+    {
+        public float PerformSubstraction(float nb1, float nb2)
+        {
+            return nb1 - nb2;
+        }
+    }
+
+    public class OperationMultiplication
+    {
+        public float PerformationMultiplication(float nb1, float nb2)
+        {
+            return nb1 * nb2;
+        }
+    }
+
+    public class OperationDivision
+    {
+        public float PerformationDivision(float nb1, float nb2)
+        {
+            return nb1 / nb2;
+        }
+    }
+
