63c2cb28685691b557577a34eaa33085c9544e39
mcn19001 <mcn19001@LEC-CZC5402D7Q>
2019-03-13 10:08:18 +0100

    Calculator design, buttons working

--- CalculatorGroup2/CalculatorGroup2/Form1.cs
+++ CalculatorGroup2/CalculatorGroup2/Form1.cs
@@ -12,9 +12,207 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        protected Boolean OperationIsPressed;
         public Form1()
         {
             InitializeComponent();
+            OperationIsPressed = false;
+            buttonPlus.Enabled = false;
+            buttonMinus.Enabled = false;
+            buttonDivision.Enabled = false;
+            buttonEqual.Enabled = false;
+            buttonDot.Enabled = false;
+            buttonMultiplication.Enabled = false;
+            
+        }
+        private void EnableAllButtons()
+        {
+            buttonPlus.Enabled = true;
+            buttonMinus.Enabled = true;
+            buttonDivision.Enabled = true;
+            buttonEqual.Enabled = true;
+            buttonDot.Enabled = true;
+            buttonMultiplication.Enabled = true;
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            if(OperationIsPressed==true) {
+                Screen.Text = Screen.Text.Substring(0, Screen.Text.Length - 3);
+                Screen.AppendText(" + ");
+                EnableAllButtons();
+                buttonPlus.Enabled = false;
+
+            }
+                
+            else {
+                OperationIsPressed = true;
+                EnableAllButtons();
+                buttonPlus.Enabled = false;
+                Screen.AppendText(" + ");
+            }
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+
+            Screen.AppendText("0");
+            OperationIsPressed = false;
+            EnableAllButtons();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("1");
+            OperationIsPressed = false;
+            EnableAllButtons();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("2");
+            OperationIsPressed = false;
+            EnableAllButtons();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("3");
+            OperationIsPressed = false;
+            EnableAllButtons();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("4");
+            OperationIsPressed = false;
+            EnableAllButtons();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("5");
+            OperationIsPressed = false;
+            EnableAllButtons();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("6");
+            OperationIsPressed = false;
+            EnableAllButtons();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("7");
+            OperationIsPressed = false;
+            EnableAllButtons();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("8");
+            OperationIsPressed = false;
+            EnableAllButtons();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Screen.AppendText("9");
+            OperationIsPressed = false;
+            EnableAllButtons();
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            if (OperationIsPressed == true)
+            {
+                Screen.Text = Screen.Text.Substring(0, Screen.Text.Length - 3);
+                Screen.AppendText(" - ");
+                EnableAllButtons();
+                buttonMinus.Enabled = false;
+
+            }
+
+            else {
+                OperationIsPressed = true;
+                EnableAllButtons();
+                buttonMinus.Enabled = false;
+                Screen.AppendText(" - ");
+            }
+            
+        }
+
+        private void buttonMultiplication_Click(object sender, EventArgs e)
+        {
+            if (OperationIsPressed == true)
+            {
+                Screen.Text = Screen.Text.Substring(0, Screen.Text.Length - 3);
+                Screen.AppendText(" X ");
+                EnableAllButtons();
+                buttonMultiplication.Enabled = false;
+
+            }
+
+            else
+            {
+                OperationIsPressed = true;
+                EnableAllButtons();
+                buttonMultiplication.Enabled = false;
+                Screen.AppendText(" X ");
+            }
+
+        }
+
+        private void buttonDivision_Click(object sender, EventArgs e)
+        {
+            if (OperationIsPressed == true)
+            {
+                Screen.Text = Screen.Text.Substring(0, Screen.Text.Length - 3);
+                Screen.AppendText(" / ");
+                EnableAllButtons();
+                buttonDivision.Enabled = false;
+
+            }
+
+            else
+            {
+                OperationIsPressed = true;
+                EnableAllButtons();
+                buttonDivision.Enabled = false;
+                Screen.AppendText(" / ");
+            }
+        }
+
+        private void buttonDeleteAll_Click(object sender, EventArgs e)
+        {
+            Screen.Text = String.Empty;
+            EnableAllButtons();
+        }
+
+        private void buttonDeleteOne_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(Screen.Text)) { 
+            if(OperationIsPressed==true)
+            {
+                Screen.Text = Screen.Text.Substring(0, Screen.Text.Length - 3);
+                OperationIsPressed = false;
+                EnableAllButtons();
+            }
+            else
+            {
+                Screen.Text = Screen.Text.Substring(0, Screen.Text.Length - 1);
+                EnableAllButtons();
+            }
+            }
         }
     }
 }
