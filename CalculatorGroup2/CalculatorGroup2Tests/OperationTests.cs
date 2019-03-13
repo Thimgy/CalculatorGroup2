cac5270700e8fb37f6009443c50184aee339f54a
mcn19001 <mcn19001@LEC-CZC5402D7Q>
2019-03-13 11:02:14 +0100

    Tests for individual methods (sum,multi,etc)

--- CalculatorGroup2/CalculatorGroup2Tests/OperationTests.cs
+++ CalculatorGroup2/CalculatorGroup2Tests/OperationTests.cs
@@ -10,10 +10,35 @@ namespace CalculatorGroup2Tests
         [TestMethod]
         public void TestAddition()
         {
-            float a = 5, b = 2;
+            float a = -10, b = 20;
             float result = Operation.OperationAddition.PerformAddition(a, b);
 
-            Assert.AreEqual(7, result);
+            Assert.AreEqual(10, result);
+        }
+        [TestMethod]
+        public void TestSubstraction()
+        {
+            float a = 20, b = -10;
+            float result = Operation.OperationSubstraction.PerformSubstraction(a, b);
+
+            Assert.AreEqual(30, result);
+        }
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            float a = -2, b = -3;
+            float result = Operation.OperationMultiplication.PerformationMultiplication(a, b);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            float a = 10, b = 2;
+            float result = Operation.OperationDivision.PerformationDivision(a, b);
+     
+            Assert.AreEqual(5, result);
         }
     }
 }
