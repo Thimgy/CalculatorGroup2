151f02afe19ebeefb471196290ff8b475cb2397d
Johann Cebollado <johann.cebollado>
2019-03-13 09:40:31 +0100

    Operation implemented

---
+++ CalculatorGroup2/CalculatorGroup2/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operation
+{
+    public class OperationAddition
+    {
+        public float PerformAddition(float nb1, float nb2)
+        {
+            return nb1 + nb2;
+        }
+    }
+
+    public class OperationSubstraction
+    {
+        public float PerformSubstraction(float nb1, float nb2)
+        {
+            return nb1 - nb2;
+        }
+    }
+
+    public class OperationMultiplication
+    {
+        public float PerformationMultiplication(float nb1, float nb2)
+        {
+            return nb1 * nb2;
+        }
+    }
+
+    public class OperationDivision
+    {
+        public float PerformationDivision(float nb1, float nb2)
+        {
+            return nb1 / nb2;
+        }
+    }
+}
