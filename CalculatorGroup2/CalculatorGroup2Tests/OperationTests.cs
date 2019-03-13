a6cbc9aafeb65e1932fa99e2e816b494be0fe162
mcn19001 <mcn19001@LEC-CZC5402D7Q>
2019-03-13 14:18:14 +0100

    Commiiting the test cases

--- CalculatorGroup2/CalculatorGroup2Tests/OperationTests.cs
+++ CalculatorGroup2/CalculatorGroup2Tests/OperationTests.cs
@@ -8,15 +8,25 @@ namespace Operation.Tests
     public class OperationTests
     {
         [TestMethod()]
-        public void TestAddition()
+        public void TestAddition1()
         {
             float a = -10, b = 20;
             float result = Operation.OperationAddition.PerformAddition(a, b);
 
             Assert.AreEqual(10, result);
         }
+
+        [TestMethod()]
+        public void TestAddition2()
+        {
+            float a = 0, b = 20;
+            float result = Operation.OperationAddition.PerformAddition(a, b);
+
+            Assert.AreNotEqual(10, result);
+        }
+
         [TestMethod]
-        public void TestSubstraction()
+        public void TestSubstraction1()
         {
             float a = 20, b = -10;
             float result = Operation.OperationSubstraction.PerformSubstraction(a, b);
@@ -24,7 +34,15 @@ namespace Operation.Tests
             Assert.AreEqual(30, result);
         }
         [TestMethod]
-        public void TestMultiplication()
+        public void TestSubstraction2()
+        {
+            float a = 20, b = 0;
+            float result = Operation.OperationSubstraction.PerformSubstraction(a, b);
+
+            Assert.AreNotEqual(10, result);
+        }
+        [TestMethod]
+        public void TestMultiplication1()
         {
             float a = -2, b = -3;
             float result = Operation.OperationMultiplication.PerformationMultiplication(a, b);
@@ -33,12 +51,31 @@ namespace Operation.Tests
         }
 
         [TestMethod]
-        public void TestDivision()
+        public void TestMultiplication2()
+        {
+            float a = 2, b = 0;
+            float result = Operation.OperationMultiplication.PerformationMultiplication(a, b);
+
+            Assert.AreNotEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestDivision1()
         {
             float a = 10, b = 2;
             float result = Operation.OperationDivision.PerformationDivision(a, b);
      
             Assert.AreEqual(5, result);
         }
+        [TestMethod]
+        public void TestDivision2()
+        {
+            float a = 1, b = 2;
+            float result = Operation.OperationDivision.PerformationDivision(a, b);
+
+            Assert.AreNotEqual(0, result);
+        }
+
+
     }
 }
